f96d1f0656b369c1e1ebe2b2ce4f281611a9b46b
Karl <pastranakd@gmail.com>
2021-06-28 18:00:12 +0800

    2nd day part 2 commit
    Creation of settings page....

---
+++ client/src/pages/settings/Settings.jsx
@@ -0,0 +1,35 @@
+import "./Settings.css"
+import Sidebar from "../../components/sidebar/Sidebar"
+
+export default function settings() {
+    return (
+        <div className = "settings" >
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update Your Account</span>
+                    <span className="settingsDeleteTitle">Delete Account</span>
+                </div>
+                <form className="settingsForm">
+                    <label>Profile Picture</label>
+                    <div className="settingsPP">
+                        <img src = "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=500" alt = ""/>
+
+                        <label htmlFor = "fileInput">
+                            <i className = "settingsPPIcon far fa-user-circle"></i>
+                        </label>
+
+                        <input type = "file" id = "fileInput" style = {{display: "none"}}/>
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder = "Stephanie" />
+                    <label>Email</label>
+                    <input type="email" placeholder = "stephanie@gmail.com" />
+                    <label>Password</label>
+                    <input type="password" />
+                    <button className="settingsSubmit">Update</button>
+                </form>
+            </div>
+            <Sidebar />
+        </div>
+    )
+}
