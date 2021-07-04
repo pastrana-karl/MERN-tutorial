2ec68809ad9abbe2f08265f343103c54b8e2a22d
Karl <pastranakd@gmail.com>
2021-07-04 11:35:56 +0800

    6th Day commit
    Creation of authentication API function...

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, 
@@ -12,6 +14,8 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(console.log("connected to mongodb"))
 .catch((err)=>console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", ()=> 
 {
