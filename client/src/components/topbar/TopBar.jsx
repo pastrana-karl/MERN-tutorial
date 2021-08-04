d21aa488f4fbb94b287206c39449c18b7943c5d2
Karl <pastranakd@gmail.com>
2021-08-04 11:08:05 +0800

    11th day commit..
    Login and register function with UI connection done...

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
 import './TopBar.css'
 
 export default function TopBar() {
-    const user = true;
+    const { user, dispatch } = useContext(Context);
+
+    const handleLogout = () => {
+        dispatch({ type: "LOGOUT" })
+    }
     return (
         <div className = "Top">
             <div className="topLeft"> <i className="topIcon fab fa-facebook-square"></i> <i className="topIcon fab fa-twitter-square"></i> <i className="topIcon fab fa-pinterest-square"></i> <i className="topIcon fab fa-instagram-square"></i> </div>
@@ -12,11 +18,11 @@ export default function TopBar() {
                     <li className="topListItem"><Link to = "/" className = "link" >ABOUT</Link></li>
                     <li className="topListItem"><Link to = "/" className = "link" >CONTACT</Link></li>
                     <li className="topListItem"><Link to = "/write" className = "link" >WRITE</Link></li>
-                    {user && <li className="topListItem"><Link to = "/" className = "link" >LOGOUT</Link></li>}
+                    <li className="topListItem" onClick = {handleLogout}>{user && "LOGOUT"}</li>
                 </ul>
             </div>
             <div className="topRight">
-                {user ? <img className = "topImg" src = "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt = ""></img>
+                {user ? <img className = "topImg" src = { user.profilePic } alt = ""></img>
                 : (
                     <ul className = "topList" >
                         <li className = "topListItem" ><Link to = "/Login" className = "link" >LOGIN</Link></li>
