147cabc2ecedf8e02fd465efba3d60886d52d37c
Karl <pastranakd@gmail.com>
2021-08-05 11:23:17 +0800

    12th day commit (morning)
    Finished deleting and updating post function....

--- api/index.js
+++ api/index.js
@@ -7,9 +7,11 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname,"/images")));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, 
@@ -21,14 +23,14 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const storage = multer.diskStorage({
     destination:(req,file,cb) => {
-        cb(null,"images");
+        cb(null, "images");
     },
     filename:(req,file,cb) => {
-        cb(null,req.body.name);
+        cb(null, req.body.name);
     },
 });
 
-const upload = multer({ storage:storage });
+const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"),(req,res) => {
     res.status(200).json("File has been uploaded");
